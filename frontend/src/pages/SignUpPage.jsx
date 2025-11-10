465f7cb673c2aa150d6a191b19b2eadb8a5dc53b
Aditya Chavan <adityakchavan23@gmail.com>
2025-11-10 22:07:47 +0530

    feat(frontend): add signup and login pages

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -13,10 +15,24 @@ const SignUpPage = () => {
 
   const { signup, isSigningUp } = useAuthStore();
 
-  const validateForm = () => {};
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email))
+      return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6)
+      return toast.error("Password must be at least 6 characters");
+
+    return true;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const success = validateForm();
+
+    if(success === true) signup(formData);
   };
 
   return (
@@ -136,6 +152,12 @@ const SignUpPage = () => {
           </div>
         </div>
       </div>
+      {/* right side */}
+
+      <AuthImagePattern
+        title="Be part of the conversation"
+        subtitle="Chat, share, and stay updated with the people you value."
+      />
     </div>
   );
 };
